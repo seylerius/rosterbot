cd699af33f6ebcf667e91658e82ab068bc888cb5
Seylerius <seylerius@publicinquisition.com>
2016-12-03 17:15:45 -0800

    Adding libraries: loco, clj-pdf, google-apps-clj
    The following libraries have been added to RosterBot:
    
    + aengelberg/loco           :: Constraint programming
    + yogthos/clj-pdf           :: PDF generation
    + SparkFund/google-apps-clj :: Google Calendar sync

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [loco "0.3.1"]
+                 [google-apps-clj "0.5.3"]
+                 [clj-pdf "2.2.8"]]
   :main ^:skip-aot com.seriouslyseylerius.rosterbot
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
