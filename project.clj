ce8465df609d07dea573eea3604acf77240f6346
Seylerius <seylerius@publicinquisition.com>
2016-12-10 02:08:49 -0800

    Shift constraint functions added

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
 (defproject com.seriouslyseylerius/rosterbot "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A tool for generating duty rosters"
+  :url "http://github.com/seylerius/rosterbot"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [loco "0.3.1"]
                  [google-apps-clj "0.5.3"]
                  [clj-pdf "2.2.8"]
-                 [clj-time "0.12.2"]]
+                 [clj-time "0.12.2"]
+                 [funcool/cuerdas "2.0.1"]]
   :main ^:skip-aot com.seriouslyseylerius.rosterbot
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/com/seriouslyseylerius/rosterbot.clj
+++ src/com/seriouslyseylerius/rosterbot.clj
@@ -1,11 +1,27 @@
 (ns com.seriouslyseylerius.rosterbot
   (:use loco.core
         loco.constraints)
-  (:require [clj-time.core :as t]
+  (:require [loco.automata :as a]
+            [clj-time.core :as t]
             [clj-time.periodic :as p]
             [clj-time.predicates :as pr]
-            [clj-time.format :as f])
+            [clj-time.format :as f]
+            [cuerdas.core :as s])
   (:gen-class))
+
+(defn all-shift-vars
+  "Return all valid team-member/day pairs"
+  [team dates]
+  (for [n (range (count team))
+        d dates]
+    [:shift n d]))
+
+(defn shift-var-declarations
+  "Define shift variables for each team-member/day pair"
+  [team shifts dates]
+  (let [shift-ids (concat [-1 -2 0] (map :id shifts))]
+    (for [v (all-shift-vars team dates)]
+      ($in v shift-ids))))
 
 (defn date-range
   "Given two dates, return the inclusive range between them"
@@ -19,7 +35,8 @@
   (cond
     ;; For daily recurrence, verify that `day` is `:period` days from `start`
     (= (shift :recurrence) :daily)
-    (= 0 (mod (t/in-days (t/interval start day)) (shift :period)))
+    (and (= 0 (mod (t/in-days (t/interval start day)) (shift :period)))
+         (not (contains? (shift :exceptions) day)))
 
     ;; For weekly recurrence, we need to verify that we're in a valid week
     ;; (by backing up `start` to the preceding Sunday, then checking `:period`)
@@ -29,12 +46,70 @@
           start (t/minus start (t/days dow))]
       (and
        (= 0 (mod (t/in-weeks (t/interval start day)) (shift :period)))
-       (some #(= dow %) (shift :days))))))
+       (some #(= dow %) (shift :days))
+       (not (contains? (shift :exceptions) day))))))
 
-(defn make-day
-  "Generate variables and constraints for a day's shifts and employees"
-  [day shifts employees start]
-  (map ))
+(defn shift-reducer
+  [acum [ks c]]
+  (assoc-in acum ks (max (get-in acum ks 0) c)))
+
+(defn shift-counter
+  "Given shifts and a date-range, return days mapped to shift counts"
+  [dates shifts]
+  (let [day-shifts (for [day dates
+                           shift shifts
+                           :when (valid-shift day shift (first dates))]
+                       [[day (shift :id)] (shift :count)])]
+    (reduce shift-reducer {} day-shifts)))
+
+(defn day-constraint
+  "Given a team, a day and a map of shift-counts, return a cardinality constraint"
+  [team day shift-counts]
+  (let [column (for [n (range (count team))]
+                 [:shift n day])]
+    ($cardinality column shift-counts)))
+
+(defn stringify-shifts
+  [shifts]
+  (let [str-shifts (s/join "|" (for [shift shifts]
+                                 (str "<" shift ">")))]
+       (str "(" str-shifts ")")))
+
+(defn shift-shorthand
+  [shift shifts]
+  (let [shifts (conj (set shifts) 0 -1 -2)
+        other (stringify-shifts (disj shifts shift))
+        out (stringify-shifts [0 -1 -2])
+        off (stringify-shifts [0 -1])
+        shift (str "<" shift ">")]
+    [shifts other out off]))
+
+(defn consecutive-shift-constraint
+  "Return an automaton requiring at least `count` shifts in a row"
+  [shifts shift count]
+  (let [[shifts other out off shift] (shift-shorthand shift shifts)]
+    (a/string->automaton
+     (str other "*"
+          "(" (apply str (repeat count shift))
+          other "*" ")" "*"))))
+
+(defn subsequent-shift-constraint
+  "Return an automaton requiring `shift` be followed by `count` of another"
+  ([shifts shift follower]
+   (subsequent-shift-constraint shifts shift follower 1))
+  ([shifts shift follower count]
+   (let [[shifts other out off shift] (shift-shorthand shift shifts)]
+     (a/string->automaton
+      (str other "*"
+           shift
+           (cond
+             (= follower :off) (apply str (repeat count off))
+             (= follower :out) (apply str (repeat count out))
+             (= follower :other) (apply str (repeat count other))
+             (seq? follower) (apply str
+                                    (repeat count
+                                            (stringify-shifts
+                                             (set follower))))))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
